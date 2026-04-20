a16c6636749b1414862595b1ccd56b88f7d99ba9
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2026-04-20 12:37:09 +0200

    add: changed default color

--- app/src/main/java/it/unibo/trace/ui/theme/Color.kt
+++ app/src/main/java/it/unibo/trace/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package it.unibo.trace.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val black = Color(0xFF000000)
+val white = Color(0xFFFFFFFF)
+val CyanPrimary = Color(0xFF00D4EC)
+val CyanLight = Color(0xFFE0F7FA)
+var CyanDeep = Color(0xFF004D57)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val RedNegative = Color(0xFFD32F2F)
+val GrayNeutral = Color(0xFF757575)
